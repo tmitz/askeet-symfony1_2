6c2e3d8d25a61a7ed09bb4984aad02609ff83874
Kiske <tmitsumune@gmail.com>
2008-11-21 14:49:47 +0900

    removed not required file rebuild Model file

--- lib/form/base/BaseAnswerForm.class.php
+++ lib/form/base/BaseAnswerForm.class.php
@@ -5,7 +5,7 @@
  *
  * @package    symfony
  * @subpackage form
- * @author     Your name here
+ * @author     Tomohiro MITSUMUNE<tmitsumune@gmail.com>
  * @version    SVN: $Id: sfPropelFormGeneratedTemplate.php 12815 2008-11-09 10:43:58Z fabien $
  */
 class BaseAnswerForm extends BaseFormPropel
